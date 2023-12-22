b3d2d4376df16d19faf075bf8fac6f8579d78c62
claycat <catenarywkd@naver.com>
2023-12-22 23:00:15 +0900

    [DEV] basic server websocket settings

--- server/src/main/java/com/claycat/customtetris/config/WebSocketConfig.java
+++ server/src/main/java/com/claycat/customtetris/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.claycat.customtetris.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-endpoint").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
     }
 
     @Override
--- server/src/main/java/com/claycat/customtetris/controller/WebSocketController.java
+++ server/src/main/java/com/claycat/customtetris/controller/WebSocketController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
-    @MessageMapping("/send-message")
-    @SendTo("/topic/messages")
+    @MessageMapping("/send-message") //send message to
+    @SendTo("/topic/messages") //topic
     public String processMessageFromClient(String message) throws Exception {
         System.out.println("message = " + message);
         return "Processed message from server " + message;
