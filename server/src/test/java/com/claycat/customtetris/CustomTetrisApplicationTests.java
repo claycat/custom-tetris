238dcd9380bb24f5825757935e9a1b2b54349fba
claycat <catenarywkd@naver.com>
2023-12-22 16:39:51 +0900

    [DEV] init springboot

---
+++ server/src/test/java/com/claycat/customtetris/CustomTetrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claycat.customtetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTetrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
